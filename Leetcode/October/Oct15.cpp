b1aed84dca1375f8cbd0354a592ce3e56d95ad05
Esha Sachan <53705020+eshasachan18@users.noreply.github.com>
2020-10-15 22:22:48 +0530

    Create Oct15.cpp

---
+++ Leetcode/October/Oct15.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {k=k%nums.size();
+        int i=0,j=nums.size()-1;
+        while(i<j)
+        {
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;j--;
+        }
+        i=0;j=k-1;
+        while(i<j)
+        {
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;j--;
+        }
+        i=k;j=nums.size()-1;
+        while(i<j)
+        {
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;j--;
+        }
+        
+        
+    }
+};
