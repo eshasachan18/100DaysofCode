45b73ac6c2380e37e699cce6cc70e9c17c8de2b8
Esha Sachan <53705020+eshasachan18@users.noreply.github.com>
2020-10-17 23:53:50 +0530

    Create Backspace String Compare.cpp

---
+++ Leetcode/Problems/Backspace String Compare.cpp
@@ -0,0 +1,115 @@
+/***......................................you are alive at this moment and no one can steal that from you.....................................................................***/
+#define FAST cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(0);
+#define MAX3(a,b,c) (a>b?(a>c?a:c):(b>c?b:c))
+#define MIN3(a,b,c) (a<b?(a<c?a:c):(b<c?b:c))
+#define len(str) int len=str.length()
+#define toStr(n,st) ({\
+ostringstream s;\
+s.clear();\
+s<<n;\
+st=s.str();\
+})
+/*_____________define and inputs______________*/
+#define ll(n) long long n; cin>>n;
+#define l(n) long n; cin>>n;
+#define inti(n) int n;cin>>n;
+#define CC int test;\
+cin>>test;\
+while(test--)
+#define YES if(!flag) cout<<"YES\n";
+#define NO else cout<<"NO\n";
+/*** Loops ***/
+#define forIn(arr, num) for(long long i = 0; i < num; i++) cin >> arr[i];
+#define for0(num) for(long long i = 0; i < num; i++)
+#define vpnt(ans) for(ll i = 0; i < ans.size(); i++) cout << ans[i] << (i + 1 < ans.size() ? ' ' : '\n');
+/*** Abbrevations **/
+#define veci(v) vector<int>v;
+#define all(v) (v).begin(), (v).end()
+#define vecs(v) vector <string>v;
+#define vecll(v) vector<long long>v;
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define mem(name, value) memset(name, value, sizeof(name))
+#define srt(v) sort(all(v))
+#define srtGreat(v) sort(all(v), greater<ll>())
+/*** Some Prints ***/
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define pf(s) cout<<s<<endl;
+/*** STLs ***/
+#include<bits/stdc++.h>
+ /***...............................................You see you things.You keep quiet about them.And you understand...........................................................***/
+class Solution {
+public:
+     bool backspaceCompare(string S, string T)
+     {
+        if(build(S)==build(T))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+    string build(string S)
+    {
+
+         stack<char>s;string ans="";
+         for0(S.length())
+         {
+             if(s.empty())
+             {
+                 if(S[i]!='#')
+                 {
+                     s.push(S[i]);
+                 }
+             }
+             else
+             {
+                 if(S[i]=='#')
+                 {
+                     s.pop();
+                 }
+                 else
+                 {
+                     s.push(S[i]);
+                 }
+             }
+         }
+             while(!s.empty())
+             {
+              ans+=s.top();
+              s.pop();
+             }
+            
+         
+         return ans;
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
